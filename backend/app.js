5ca8de12f0cedf40d9ce935f8f12fa4749815ba9
Pavel Rezvanov <rezvanoffpavel@yandex.ru>
2024-02-03 19:37:58 +0300

    PR15

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,15 @@ const NotFoundError = require('./errors/NotFoundError');
 
 const { PORT = 3000 } = process.env;
 
+const allowedCors = [
+  'https://api.mesto.pavel.nomoredomainsmonster.ru',
+  'https://mesto.pavel.nomoredomainsmonster.ru',
+  'localhost:3000',
+];
+
+// CORS: Разрешенные методы
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
@@ -60,6 +69,30 @@ app.use('/cards', cardRoutes);
 
 app.use('*', (req, res, next) => {
   next(new NotFoundError('Нет такого маршрута'));
+});
+
+app.use('*', (req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  }
+
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
+  return next();
 });
 
 app.use(errorLogger); // подключаем логгер ошибок
