ed06ddd488497ba984d096e9a99644f6af92c6fc
Pavel Rezvanov <rezvanoffpavel@yandex.ru>
2024-02-04 04:44:21 +0300

    PR15

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -36,13 +36,13 @@ export const authorize = (password, email) => {
   })
 }; 
 
-export const checkToken = (token) => {
+export const checkToken = () => {
   return fetch(`${BASE_URL}/users/me`, {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
     }
   })
   .then((res) => {
