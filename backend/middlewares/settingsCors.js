ea88f5eb6e9fcb448a43e508f5e88f0e37ef364d
Pavel Rezvanov <rezvanoffpavel@yandex.ru>
2024-02-04 05:43:05 +0300

    PR15

--- backend/middlewares/settingsCors.js
+++ backend/middlewares/settingsCors.js
@@ -1,7 +1,7 @@
 const allowedCors = [
   'https://api.mesto.pavel.nomoredomainsmonster.ru',
   'https://mesto.pavel.nomoredomainsmonster.ru',
-  'localhost:3000',
+  'http://localhost:3000',
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
