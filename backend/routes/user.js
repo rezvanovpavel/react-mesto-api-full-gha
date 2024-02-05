f441716ca4d88067239e82c8080ed34fc3142a62
Pavel Rezvanov <rezvanoffpavel@yandex.ru>
2024-02-05 19:35:56 +0300

    PR15

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -11,7 +11,7 @@ router.get('/', getUsers);
 
 router.get('/me', getCurrentUserInfo);
 
-router.get('/:userId', celebrate({
+router.get('/me', celebrate({
   params: Joi.object().keys({
     userId: Joi.string().length(24).hex().required(),
   }),
