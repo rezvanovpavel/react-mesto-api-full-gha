0968efde82ad111b9bb9f92dbf3558f99fea8c5e
Pavel Rezvanov <rezvanoffpavel@yandex.ru>
2024-02-06 16:06:15 +0300

    4.0

--- backend/app.js
+++ backend/app.js
@@ -74,7 +74,7 @@ app.use(errors()); // обработчик ошибок celebrate
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = statusCode === 500 ? "На сервере произошла ошибка" : err.message;
+  const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
   res.status(statusCode).send({ message });
   next();
 });
